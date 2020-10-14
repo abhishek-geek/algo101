d5aa96e23f70f27ebbcfcaeba9a6d7e351719ebf
Somya Prajapati <64978837+somya51p@users.noreply.github.com>
2020-10-14 14:36:10 +0530

    Create selectionSort.py

---
+++ selectionSort.py
@@ -0,0 +1,14 @@
+#the loop checks cond and performs sorting, this method is SELECTION SORT
+#time complexity is n^2
+
+A = ['t','u','t','o','r','i','a','l']
+for i in range(len(A)):
+   min_= i
+   for j in range(i+1, len(A)):
+      if A[min_] > A[j]:
+         min_ = j
+   #swap
+A[i], A[min_] = A[min_], A[i]
+# main
+for i in range(len(A)):
+   print(A[i])
